43336b1b1a5f805106c344408619080a12a72052
Amy Pierce <diablokitty@gmail.com>
2022-08-17 23:21:15 -0700

    still need last challenge

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -79,7 +79,7 @@ Read the MDN documentation on String.charCodeAt() if necessary.
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
-const charCode = (arr) => arr.map(item => arr.CharCodeAt(item));
+const charCode = (arr) => arr.map(ltr => ltr.charCodeAt());
 
 
 /* ------------------------------------------------------------------------------------------------
@@ -93,7 +93,15 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------ */
 
 const evenOdd = (arr) => {
-  // Solution code here...
+  return arr.map((num) => {
+    if(typeof num === 'number'){
+      return num % 2 === 0 ?'even' : 'odd';
+    }
+    else {
+      return 'N/A';
+    }
+
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -138,9 +146,9 @@ const snorlaxAbilities = {
   weight: 4600,
 };
 
-const extractAbilities = (arr) => {
-  // Solution code here...
-};
+// const extractAbilities = (arr) => newArr.push(arr.map => return Snorlax.ability){
+//   // Solution code here...
+// };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
@@ -186,6 +194,7 @@ const snorlaxStats = {
 };
 
 const extractStats = (arr) => {
+  return arr;
   // Solution code here...
 };
 
@@ -251,14 +260,14 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
