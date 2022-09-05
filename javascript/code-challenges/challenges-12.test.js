fd42bbabb5bc8fb9584a5151357aa872cc087c04
Amy Pierce <diablokitty@gmail.com>
2022-09-05 00:21:43 -0700

    all tests passing

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -7,13 +7,13 @@ Write a function named replaceZeros that, given a string, uses Regex to replace 
 ------------------------------------------------------------------------------------------------ */
 
 const replaceZeros = (string) => {
-  string.replace(/0/, 'zero');
+  let reg = (/0/g);
   console.log(string);
-  return string;
+  return string.replace(reg, 'zero');
   // Solution code here...
 };
 
-/* ------------------------------------------------------------------------------------------------
+/* ------------------------------------------.------------------------------------------------------
 CHALLENGE 2
 
 Write a function named validatePin that uses a regular expression pattern to validate a PIN.
@@ -21,8 +21,10 @@ Write a function named validatePin that uses a regular expression pattern to val
 If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
-const validatePin = (pin) => pin.test(/\d\d\d\d/g);
-
+const validatePin = (pin) => {
+  let pinPat = /^\d{4}$/;
+  return pinPat.test(pin);
+};
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
@@ -32,7 +34,9 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 ------------------------------------------------------------------------------------------------ */
 
 const validateWord = (word) => {
-  // Solution code here...
+  let pattern = /^[a-zA-Z]{5,10}$/;
+  return pattern.test(word);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -44,7 +48,8 @@ If it does, return true. If not, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const hasNumber = (string) => {
-  // Solution code here...
+  let reg = /[a-zA-Z]\d/g;
+  return reg.test(string);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -64,7 +69,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  let emailPat = /^[a-zA-Z0-9]+\.?[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|net|org)$/;
+  return emailPat.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -89,7 +95,8 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+  let reg = /^(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
+  return reg.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
